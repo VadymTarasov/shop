cf5da925d077c54c4e111d3ae2ffe7ae64034c7c
VadymTarasov <90509364+VadymTarasov@users.noreply.github.com>
2021-11-20 15:06:17 +0200

    надпись оповищения о заказе сделал зеленую

--- resources/views/pet-shop/checkout.blade.php
+++ resources/views/pet-shop/checkout.blade.php
@@ -11,7 +11,7 @@
 
 @section('content')
     @if($messageSuccessOrder)
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>{{$messageSuccessOrder}}</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
