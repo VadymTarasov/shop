8c1b384aa0a8dfb87d4e64c29e32414415fa0d8d
VadymTarasov <90509364+VadymTarasov@users.noreply.github.com>
2021-11-20 15:24:51 +0200

    убрал лишний блоки в checkout

--- resources/views/pet-shop/checkout.blade.php
+++ resources/views/pet-shop/checkout.blade.php
@@ -41,9 +41,9 @@
 
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
-                                        <h5 class="panel-title"><span>2</span> <a data-toggle="collapse" data-parent="#faq" href="#payment-2">billing information</a></h5>
+                                        <h5 class="panel-title"><span>1</span> <a data-toggle="collapse" data-parent="#faq" href="#payment-1">billing information</a></h5>
                                     </div>
-                                    <div id="payment-2" class="panel-collapse collapse">
+                                    <div id="payment-1" class="panel-collapse collapse">
                                         <form method="post" action="{{route('pet-shop/make-order')}}">
                                             @csrf
                                         <div class="panel-body">
@@ -157,179 +157,12 @@
                                         </form>
                                     </div>
                                 </div>
+
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
-                                        <h5 class="panel-title"><span>3</span> <a data-toggle="collapse" data-parent="#faq" href="#payment-3">shipping information</a></h5>
+                                        <h5 class="panel-title"><span>2</span> <a data-toggle="collapse" data-parent="#faq" href="#payment-2">History Of Order</a></h5>
                                     </div>
-                                    <div id="payment-3" class="panel-collapse collapse ">
-                                        <div class="panel-body">
-                                            <div class="shipping-information-wrapper">
-                                                <div class="shipping-info-2">
-                                                    <span> Address will go here. </span>
-                                                    <span>T: 012 345 678 </span>
-                                                </div>
-                                                <div class="edit-address">
-                                                    <a href="#">Edit Address</a>
-                                                </div>
-                                                <div class="billing-select">
-                                                    <select class="email s-email s-wid">
-                                                        <option>Select Your Address</option>
-                                                        <option>Add New Address</option>
-                                                        <option>Boot Experts, Bangladesh</option>
-                                                    </select>
-                                                </div>
-                                                <div class="ship-wrapper">
-                                                    <div class="single-ship">
-                                                        <input type="checkbox" checked="" value="address" name="address">
-                                                        <label>Use Billing Address</label>
-                                                    </div>
-                                                </div>
-                                                <div class="billing-back-btn">
-                                                    <div class="billing-back">
-                                                        <a href="#"><i class="ti-arrow-up"></i> back</a>
-                                                    </div>
-                                                    <div class="billing-btn">
-                                                        <button type="submit">Continue</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h5 class="panel-title"><span>4</span> <a data-toggle="collapse" data-parent="#faq" href="#payment-4">Shipping method</a></h5>
-                                    </div>
-                                    <div id="payment-4" class="panel-collapse collapse ">
-                                        <div class="panel-body">
-                                            <div class="shipping-method-wrapper">
-                                                <div class="shipping-method">
-                                                    <p>Flat Rate</p>
-                                                    <p>Fixed $40.00</p>
-                                                </div>
-                                                <div class="billing-back-btn">
-                                                    <div class="billing-back">
-                                                        <a href="#"><i class="ti-arrow-up"></i> back</a>
-                                                    </div>
-                                                    <div class="billing-btn">
-                                                        <button type="submit">Continue</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h5 class="panel-title"><span>5</span> <a data-toggle="collapse" data-parent="#faq" href="#payment-5">payment information</a></h5>
-                                    </div>
-                                    <div id="payment-5" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <div class="payment-info-wrapper">
-                                                <div class="ship-wrapper">
-                                                    <div class="single-ship">
-                                                        <input type="radio" checked="" value="address" name="address">
-                                                        <label>Check / Money order </label>
-                                                    </div>
-                                                    <div class="single-ship">
-                                                        <input type="radio" value="dadress" name="address">
-                                                        <label>Credit Card (saved) </label>
-                                                    </div>
-                                                </div>
-                                                <div class="payment-info">
-                                                    <div class="row">
-                                                        <div class="col-lg-6 col-md-6">
-                                                            <div class="billing-info">
-                                                                <label>Name on Card </label>
-                                                                <input type="text">
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-lg-6 col-md-6">
-                                                            <div class="billing-select card-mrg">
-                                                                <label>Credit Card Type</label>
-                                                                <select>
-                                                                    <option>American Express</option>
-                                                                    <option>Visa</option>
-                                                                    <option>MasterCard</option>
-                                                                    <option>Discover</option>
-                                                                </select>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-lg-12 col-md-12">
-                                                            <div class="billing-info">
-                                                                <label>Credit Card Number </label>
-                                                                <input type="text">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="expiration-date">
-                                                        <label>Expiration Date </label>
-                                                        <div class="row">
-                                                            <div class="col-lg-6 col-md-6">
-                                                                <div class="billing-select month-mrg">
-                                                                    <select>
-                                                                        <option>Month</option>
-                                                                        <option>January</option>
-                                                                        <option>February</option>
-                                                                        <option> March</option>
-                                                                        <option>April</option>
-                                                                        <option> May</option>
-                                                                        <option>June</option>
-                                                                        <option>July</option>
-                                                                        <option>August</option>
-                                                                        <option>September</option>
-                                                                        <option> October</option>
-                                                                        <option> November</option>
-                                                                        <option> December</option>
-                                                                    </select>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-lg-6 col-md-6">
-                                                                <div class="billing-select">
-                                                                    <select>
-                                                                        <option>Year</option>
-                                                                        <option>2015</option>
-                                                                        <option>2016</option>
-                                                                        <option>2017</option>
-                                                                        <option>2018</option>
-                                                                        <option>2019</option>
-                                                                        <option>2020</option>
-                                                                        <option>2021</option>
-                                                                        <option>2022</option>
-                                                                        <option>2023</option>
-                                                                        <option>2024</option>
-                                                                        <option>2025</option>
-                                                                    </select>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-lg-12 col-md-12">
-                                                            <div class="billing-info">
-                                                                <label>Card Verification Number</label>
-                                                                <input type="text">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="billing-back-btn">
-                                                    <div class="billing-back">
-                                                        <a href="#"><i class="ti-arrow-up"></i> back</a>
-                                                    </div>
-                                                    <div class="billing-btn">
-                                                        <button type="submit">Continue</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h5 class="panel-title"><span>6</span> <a data-toggle="collapse" data-parent="#faq" href="#payment-6">Order Review</a></h5>
-                                    </div>
-                                    <div id="payment-6" class="panel-collapse collapse">
+                                    <div id="payment-2" class="panel-collapse collapse">
                                         <div class="panel-body">
                                             <div class="order-review-wrapper">
                                                 <div class="order-review">
